c2af5ed3eca51272faa5ff6c058df573df63d2d8
Cristiankoch <cristiankoch@agenciawing.com.br>
2024-01-31 16:16:51 -0300

    feat: testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss';
+import Header from './components/header';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-wrapper">
+        <h1 className="teste">Hellow word</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
